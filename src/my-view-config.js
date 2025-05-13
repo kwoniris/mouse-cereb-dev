71f9fb62f8485de9650ac50f8b3783a332144fda
Iris Kwon <seeuniris@gmail.com>
2025-05-13 15:37:15 -0400

    forth commit

--- src/my-view-config.js
+++ src/my-view-config.js
@@ -1,145 +1,143 @@
-const myViewConfig = {
-  "version": "1.0.15",
-  "name": "E11 STARmap slice 1",
-  "description": "",
-  "datasets": [
+// config.js
+
+export const myViewConfig = {
+  version: "1.0.15",
+  name: "E11 STARmap slice 1",
+  description: "",
+  datasets: [
     {
-      "uid": "A",
-      "name": "E11",
-      "files": [
+      uid: "A",
+      name: "E11",
+      files: [
         {
-          "fileType": "anndata.zarr",
-          "url": "public/data//A/0/815a9617-ab94-4ff0-a6b4-6d1e5ad28e33.adata.zarr",
-          "options": {
-            "obsLocations": {
-              "path": "obsm/spatial"
+          fileType: "anndata.zarr",
+          url: "public/data//A/0/815a9617-ab94-4ff0-a6b4-6d1e5ad28e33.adata.zarr",
+          options: {
+            obsLocations: {
+              path: "obsm/spatial"
             },
-            "obsEmbedding": [
+            obsEmbedding: [
               {
-                "path": "obsm/X_umap",
-                "dims": [
-                  0,
-                  1
-                ],
-                "embeddingType": "UMAP"
+                path: "obsm/X_umap",
+                dims: [0, 1],
+                embeddingType: "UMAP"
               }
             ],
-            "obsSets": [
+            obsSets: [
               {
-                "name": "Cell Type",
-                "path": "obs/cell_types"
+                name: "Cell Type",
+                path: "obs/cell_types"
               },
               {
-                "name": "Coronal Slice A-P",
-                "path": "obs/library_id"
+                name: "Coronal Slice A-P",
+                path: "obs/library_id"
               },
               {
-                "name": "Leiden Clusters",
-                "path": "obs/leiden"
+                name: "Leiden Clusters",
+                path: "obs/leiden"
               }
             ],
-            "obsFeatureMatrix": {
-              "path": "X"
+            obsFeatureMatrix: {
+              path: "X"
             }
           }
         }
       ]
     }
   ],
-  "coordinationSpace": {
-    "dataset": {
-      "A": "A"
+  coordinationSpace: {
+    dataset: {
+      A: "A"
     },
-    "embeddingType": {
-      "A": "UMAP"
+    embeddingType: {
+      A: "UMAP"
     },
-    "spatialZoom": {
-      "A": 1.5
+    spatialZoom: {
+      A: 1.5
     },
-    "spatialTargetX": {
-      "A": 259
+    spatialTargetX: {
+      A: 259
     },
-    "spatialTargetY": {
-      "A": 500
+    spatialTargetY: {
+      A: 500
     },
-    "spatialSegmentationLayer": {
-      "A": {
-        "opacity": 1,
-        "radius": 2,
-        "visible": true,
-        "stroked": false
+    spatialSegmentationLayer: {
+      A: {
+        opacity: 1,
+        radius: 2,
+        visible: true,
+        stroked: false
       }
     }
   },
-  "layout": [
+  layout: [
     {
-      "component": "description",
-      "coordinationScopes": {
-        "dataset": "A"
+      component: "description",
+      coordinationScopes: {
+        dataset: "A"
       },
-      "x": 0.0,
-      "y": 0.0,
-      "w": 3.0,
-      "h": 3.0,
-      "props": {
-        "description": "My E11 STARmap data."
+      x: 0.0,
+      y: 0.0,
+      w: 3.0,
+      h: 3.0,
+      props: {
+        description: "My E11 STARmap data."
       }
     },
     {
-      "component": "spatial",
-      "coordinationScopes": {
-        "dataset": "A",
-        "spatialZoom": "A",
-        "spatialTargetX": "A",
-        "spatialTargetY": "A",
-        "spatialSegmentationLayer": "A"
+      component: "spatial",
+      coordinationScopes: {
+        dataset: "A",
+        spatialZoom: "A",
+        spatialTargetX: "A",
+        spatialTargetY: "A",
+        spatialSegmentationLayer: "A"
       },
-      "x": 3.0,
-      "y": 0,
-      "w": 3.0,
-      "h": 12
+      x: 3.0,
+      y: 0,
+      w: 3.0,
+      h: 12
     },
     {
-      "component": "scatterplot",
-      "coordinationScopes": {
-        "embeddingType": "A"
+      component: "scatterplot",
+      coordinationScopes: {
+        embeddingType: "A"
       },
-      "x": 6.0,
-      "y": 0,
-      "w": 3.0,
-      "h": 12,
-      "coordinationScopesBy": {}
+      x: 6.0,
+      y: 0,
+      w: 3.0,
+      h: 12
     },
     {
-      "component": "featureList",
-      "coordinationScopes": {
-        "dataset": "A"
+      component: "featureList",
+      coordinationScopes: {
+        dataset: "A"
       },
-      "x": 0.0,
-      "y": 6.0,
-      "w": 3.0,
-      "h": 6.0
+      x: 0.0,
+      y: 6.0,
+      w: 3.0,
+      h: 6.0
     },
     {
-      "component": "obsSets",
-      "coordinationScopes": {
-        "dataset": "A"
+      component: "obsSets",
+      coordinationScopes: {
+        dataset: "A"
       },
-      "x": 0.0,
-      "y": 3.0,
-      "w": 3.0,
-      "h": 3.0
+      x: 0.0,
+      y: 3.0,
+      w: 3.0,
+      h: 3.0
     },
     {
-      "component": "heatmap",
-      "coordinationScopes": {
-        "dataset": "A"
+      component: "heatmap",
+      coordinationScopes: {
+        dataset: "A"
       },
-      "x": 9.0,
-      "y": 0,
-      "w": 3.0,
-      "h": 12
+      x: 9.0,
+      y: 0,
+      w: 3.0,
+      h: 12
     }
   ],
-  "initStrategy": "auto"
+  initStrategy: "auto"
 };
