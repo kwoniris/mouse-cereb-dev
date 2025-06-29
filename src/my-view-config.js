7c9f84f72867c3fe047d499418cef983f37c52df
Iris Kwon <seeuniris@gmail.com>
2025-06-29 15:37:56 -0700

    commit

--- src/my-view-config.js
+++ src/my-view-config.js
@@ -1,6 +1,6 @@
 export const myViewConfig = {
   "version": "1.0.15",
-  "name": "E11 STARmap",
+  "name": "E11 & E13 STARmap",
   "description": "",
   "datasets": [
     {
@@ -45,26 +45,76 @@ export const myViewConfig = {
           }
         }
       ]
+    },
+    {
+      "uid": "B", 
+      "name": "E13",
+      "files": [
+        {
+          "fileType": "anndata.zarr",
+          "url":"https://kwoniris.github.io/mouse-cereb-dev/data/E13_Normalized.zarr",
+          "options": {
+            "obsLocations": {
+              "path": "obsm/spatial"
+            },
+            "obsEmbedding": [
+              {
+                "path": "obsm/X_umap",
+                "dims": [0, 1],
+                "embeddingType": "UMAP"
+              }
+            ],
+            "obsSets": [
+              {
+                "name": "Cell Type",
+                "path": "obs/cell_types"
+              },
+              {
+                "name": "Coronal Slice A-P",
+                "path": "obs/library_id"
+              },
+              {
+                "name": "Leiden Clusters",
+                "path": "obs/leiden"
+              }
+            ],
+            "obsFeatureMatrix": {
+              "path": "X"
+            }
+          }
+        }
+      ]
     }
   ],
   "coordinationSpace": {
     "dataset": {
-      "A": "A"
+      "A": "A",
+      "B":"B"
     },
     "embeddingType": {
-      "A": "UMAP"
+      "A": "UMAP",
+      "B": "UMAP"
     },
     "spatialZoom": {
-      "A": 1.5
+      "A": 1.5,
+      "B": 2.5
     },
     "spatialTargetX": {
-      "A": 259
+      "A": 259,
+      "B": 500
     },
     "spatialTargetY": {
-      "A": 500
+      "A": 500,
+      "B": 48
     },
     "spatialSegmentationLayer": {
       "A": {
+        "opacity": 1,
+        "radius": 2,
+        "visible": true,
+        "stroked": false
+      },
+      "B": {
         "opacity": 1,
         "radius": 2,
         "visible": true,
@@ -73,6 +123,7 @@ export const myViewConfig = {
     }
   },
   "layout": [
+    // E11 Views 
     {
       "component": "description",
       "coordinationScopes": {
@@ -140,6 +191,75 @@ export const myViewConfig = {
       "y": 0,
       "w": 3.0,
       "h": 12
+    },
+    // E13 Views
+    {
+      "component": "description",
+      "coordinationScopes": {
+        "dataset": "B"
+      },
+      "x": 0.0,
+      "y": 12.0,
+      "w": 3.0,
+      "h": 3.0,
+      "props": {
+        "description": "My E13 STARmap data."
+      }
+    },
+    {
+      "component": "spatial",
+      "coordinationScopes": {
+        "dataset": "B",
+        "spatialZoom": "B",
+        "spatialTargetX": "B",
+        "spatialTargetY": "B",
+        "spatialSegmentationLayer": "B"
+      },
+      "x": 3.0,
+      "y": 12.0,
+      "w": 3.0,
+      "h": 12
+    },
+    {
+      "component": "scatterplot",
+      "coordinationScopes": {
+        "embeddingType": "B"
+      },
+      "x": 6.0,
+      "y": 12.0,
+      "w": 3.0,
+      "h": 12,
+      "coordinationScopesBy": {}
+    },
+    {
+      "component": "featureList",
+      "coordinationScopes": {
+        "dataset": "B"
+      },
+      "x": 0.0,
+      "y": 18.0,
+      "w": 3.0,
+      "h": 6.0
+    },
+    {
+      "component": "obsSets",
+      "coordinationScopes": {
+        "dataset": "B"
+      },
+      "x": 0.0,
+      "y": 15.0,
+      "w": 3.0,
+      "h": 3.0
+    },
+    {
+      "component": "heatmap",
+      "coordinationScopes": {
+        "dataset": "B"
+      },
+      "x": 9.0,
+      "y": 12.0,
+      "w": 3.0,
+      "h": 3.0
     }
   ],
   "initStrategy": "auto"
