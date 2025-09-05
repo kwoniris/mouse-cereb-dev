c962acad0577f22a260f75035d62124b4a6c33c3
Iris Kwon <seeuniris@gmail.com>
2025-09-04 22:19:52 -0400

    update configs and App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { Vitessce } from 'vitessce';
 import { e11Config } from './e11-config';
 import { e13Config } from './e13-config';
+// import { e15Config } from './e15-config';
+// import { e17Config } from './e17-config';
 import { e11ScaledConfig } from './e11-scaled-config';
+import { e13ScaledConfig } from './e13-scaled-config';
+import { e15ScaledConfig } from './e15-scaled-config';
+import { e17ScaledConfig } from './e17-scaled-config';
 
 export default function App() {
   const [viewConfig, setViewConfig] = useState(null);
@@ -18,6 +23,12 @@ export default function App() {
       config = e13Config;
     } else if (configParam === 'e11-scaled') {
       config = e11ScaledConfig;
+    } else if (configParam === 'e13-scaled') {
+      config = e13ScaledConfig;
+    } else if (configParam === 'e15-scaled') {
+      config = e15ScaledConfig;
+    } else if (configParam === 'e17-scaled') {
+      config = e17ScaledConfig;
     }
     setViewConfig(config);
   }, []);
